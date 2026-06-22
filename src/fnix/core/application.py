f6ca82b1cbe4ba14258c57f8fbe686ecd79a0bb9
FinedYT <ytfined@gmail.com>
2026-06-22 21:16:17 +0500

    feat(response): add JSON response helper

--- src/fnix/core/application.py
+++ src/fnix/core/application.py
@@ -1,5 +1,7 @@
 from src.fnix.routing.router import Router
 from src.fnix.networking.listener import Listener
+from src.fnix.core.handlers import index_handler
+from src.fnix.core.handlers import about_handler
 
 class Application:
     def __init__(self):
@@ -9,6 +11,11 @@ class Application:
 
     def run(self, mode="sync"):
         self.mode = mode
+
+        if not self.router.routes:
+            self.add_route("/", self._welcome_page)
+            self.add_route("/about", self._about_page)
+
         Listener().start_server(self)
 
     def route(self, path):
@@ -24,6 +31,12 @@ class Application:
             return handler
 
         return decorator
+
+    def _welcome_page(self, request):
+        return index_handler(request)
+
+    def _about_page(self, request):
+        return about_handler(request)
 
     def post(self, path):
         def decorator(handler):
--- src/fnix/http/response.py
+++ src/fnix/http/response.py
@@ -1,3 +1,5 @@
+import json
+
 class Response:
     def __init__(self):
         self.status_code = 200
@@ -8,3 +10,10 @@ class Response:
         }
         self.server = {"Server": "Fnix"}
         self.body = "<h1>Hello Fnix</h1>"
+
+    @staticmethod
+    def json(data):
+        response = Response()
+        response.body = json.dumps(data)
+        response.headers = {"Content-Type": "application/json"}
+        return response
